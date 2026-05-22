01b5482b2702778deab88718fd5e076a51592349
laut-aro <lautaro57917@gmail.com>
2026-05-21 23:01:25 -0300

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // conexión base apuntando al backend
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
